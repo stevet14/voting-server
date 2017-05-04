eeecb265ff6fc357cde3bda30bb04f5b01be2c27
stevet14 <steve.taplin@gmail.com>
2017-05-04 23:32:08 +0100

    Second commit

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,29 +1,25 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
 
-  describe('setEntries', () => {
+  // ..
 
-    it('adds the entries to the state', () => {
-      const state = Map();
-      const entries = List.of('Trainspotting', '28 Days Later');
-      const nextState = setEntries(state, entries);
+  describe('next', () => {
+
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+      const nextState = next(state);
       expect(nextState).to.equal(Map({
-        entries: List.of('Trainspotting', '28 Days Later')
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
       }));
     });
-
-it('converts to immutable', () => {
-  const state = Map();
-  const entries = ['Trainspotting', '28 Days Later'];
-  const nextState = setEntries(state, entries);
-  expect(nextState).to.equal(Map({
-    entries: List.of('Trainspotting', '28 Days Later')
-  }));
-});
 
   });
 
