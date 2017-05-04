122edd86c1dc5111328a1fc52801047440be35ce
stevet14 <steve.taplin@gmail.com>
2017-05-04 23:43:28 +0100

    Move to next pair

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,23 +1,53 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-import {setEntries, next} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
 
-  // ..
+  // ...
 
-  describe('next', () => {
+  describe('vote', () => {
 
-    it('takes the next two entries under vote', () => {
+    it('creates a tally for the voted entry', () => {
       const state = Map({
-        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
-      });
-      const nextState = next(state);
-      expect(nextState).to.equal(Map({
         vote: Map({
           pair: List.of('Trainspotting', '28 Days Later')
         }),
-        entries: List.of('Sunshine')
+        entries: List()
+      });
+      const nextState = vote(state, 'Trainspotting');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 1
+          })
+        }),
+        entries: List()
+      }));
+    });
+
+    it('adds to existing tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 3,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Trainspotting');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
       }));
     });
 
